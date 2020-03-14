b1a1a4fa4d28abb4d2b3a84b3dc6314f56646f39
patrick <pfcoutinho@outlook.com>
2020-03-14 18:04:29 -0300

    Update main.m

--- main.m
+++ main.m
@@ -1,47 +1,14 @@
-%% BLIND SOURCE EXTRACTION BASED ON KOLMOGOROV-CHAITIN COMPLEXITY
+%% Blind source extraction (BSE) of images based on complexity measure
+% Examples of blind source extraction (BSE) based on Kolmogorov-Chaitin
+% complexity, which is estimated by means of lossless compression of data.
 %
-%   Examples of blind source extraction (BSE) based on Kolmogorov-Chaitin
-%   complexity, which is estimated by means of lossless compression of data (in
-%   this case, we use the LZW compression of TIFF images). To perform the BSE,
-%   we assume that the mixtures are more complex (i.e., can be less compressed)
-%   than the source image.
-%
-%   The scenario is as follows:
-%       * A is the full rank mixing matrix, which is given by:
-%
-%           A = [cos(\theta)  -sin(\theta)
-%                sin(\theta)   cos(\theta)]
-%
-%       * w is the extraction vector, which is:
-%
-%           w = [cos(\phi)  sin(\phi)]^T.
-%
-%   Before applying the mixing process, the images are converted to 1-D arrays.
-%   Thus, we have:
-%
-%           x = A*s,
-%
-%   where x corresponds to the mixed images and s are the souces (one is the
-%   image of interest and the other is a noisy image).
-%   The extraction process is given by:
-%
-%           y = w*x
-%
-%   where y is the recovered image.
-%
-% REFERENCES
-%   P. Pajunen, "Blind source separation using algorithmic information theory",
-%       Neurocomputing, v. 22, 1-3, p. 35-48, 1998. DOI: 
-%       doi.org/10.1016/S0925-2312(98)00048-4.
-%   D. C. Soriano, R. Suyama, and R. Attux, "Blind extraction of chaotic sources
-%       from mixtures with stochastic signals based on recurrence quantification
-%       analysis", Digital Signal Processing, v. 21, 3, p. 417-426, 2011.
+% Please, refer to the README for more information.
 %
 % CONTACT
 %   Patrick Franco Coutinho
 %   pfcoutinho@outlook.com
 %
-% Last update: Feb 20, 2020
+% Last update: Mar 14, 2020
 % ============================================================================ %
 
 close all
